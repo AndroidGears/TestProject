1ce9fce686ffd428cfc2806e17fcbd0b909f0aad
Matt York <myork@cs.ua.edu>
2014-04-05 14:14:40 -0500

    - Added TestProjectFile with helloDependencyMethod

---
+++ TestLibrary/src/main/java/com/mattyork/testlibrary/testlibrary/TestProject.java
@@ -0,0 +1,10 @@
+package com.mattyork.testlibrary.testlibrary;
+
+/**
+ * Created by matthewyork on 4/5/14.
+ */
+public class TestProject {
+    public static String helloDependency(){
+        return "Hello Dependency!";
+    }
+}
